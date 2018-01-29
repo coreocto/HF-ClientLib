0d05ad76e912a5b97a72b84979831024be4d7b52
coreocto <coreocto@hotmail.com>
2018-01-29 18:12:25 +0800

    Added Chlh2Client

---
+++ src/main/java/org/coreocto/dev/hf/clientlib/sse/chlh/Chlh2Client.java
@@ -0,0 +1,113 @@
+package org.coreocto.dev.hf.clientlib.sse.chlh;
+
+import com.skjegstad.utils.BloomFilter;
+import org.coreocto.dev.hf.clientlib.LibConstants;
+import org.coreocto.dev.hf.clientlib.parser.IFileParser;
+import org.coreocto.dev.hf.commonlib.crypto.IByteCipher;
+import org.coreocto.dev.hf.commonlib.sse.chlh.Index;
+import org.coreocto.dev.hf.commonlib.util.IBase64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chlh2Client {
+
+    private static final int BITSET_SIZE = 512 * 2;
+    private static final int EXPECTED_NUM_OF_ELEMENTS = 500 * 2;
+    private IBase64 base64;
+    private byte[] secretKey = null;
+
+    public Chlh2Client(IBase64 base64) {
+        this.base64 = base64;
+    }
+
+    public byte[] getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(byte[] secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public void KeyGen(int secParam) {
+        secretKey = new byte[secParam];
+    }
+
+    public String EncId(String docId, IByteCipher byteCipher) throws UnsupportedEncodingException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, NoSuchPaddingException, InvalidAlgorithmParameterException {
+        byte[] enc = byteCipher.encrypt(docId.getBytes(LibConstants.ENCODING_UTF8));
+        return base64.encodeToString(enc);
+    }
+
+    public String DecId(String encDocId, IByteCipher byteCipher) throws UnsupportedEncodingException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, NoSuchPaddingException, InvalidAlgorithmParameterException {
+        byte[] dec = byteCipher.decrypt(base64.decodeToByteArray(encDocId));
+        return new String(dec, LibConstants.ENCODING_UTF8);
+    }
+
+    public Index BuildIndex(InputStream is, IFileParser fileParser, String docId, IByteCipher byteCipher) throws BadPaddingException, InvalidAlgorithmParameterException, UnsupportedEncodingException, IllegalBlockSizeException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        List<String> keywords = fileParser.getText(is);
+
+        Index docIndex = new Index();
+        docIndex.setDocId(this.EncId(docId, byteCipher));
+
+        for (String keyword : keywords) {
+            char[] chars = keyword.toCharArray();
+
+            BloomFilter<String> bloomFilter = new BloomFilter(BITSET_SIZE, EXPECTED_NUM_OF_ELEMENTS);
+
+            int charLen = chars.length;
+
+            for (int j = 0; j < charLen; j++) {
+                bloomFilter.add(chars[j] + LibConstants.EMPTY_STRING + j);
+//                System.out.println(chars[j] + "," + j);
+                bloomFilter.add(chars[j] + LibConstants.EMPTY_STRING + 0);
+            }
+
+            for (int i = charLen - 1; i >= 0; i--) {
+                bloomFilter.add(chars[i] + LibConstants.EMPTY_STRING + (1 - charLen + i));
+//                System.out.println(chars[i] + "," + (1 - charLen + i));
+            }
+
+            for (int j = 0; j < EXPECTED_NUM_OF_ELEMENTS - charLen; j++) {
+                bloomFilter.add(((int) Math.random()) + "");
+            }
+
+            docIndex.getBloomFilters().add(base64.encodeToString(bloomFilter.getBitSet().toByteArray()));
+            bloomFilter.clear();
+        }
+
+        return docIndex;
+    }
+
+    public List<String> Trapdoor(String w) {
+
+        List<String> out = new ArrayList<>();
+
+        if (w == null || w.isEmpty()) {
+            return out;
+        }
+
+        char[] chars = w.toCharArray();
+
+        BloomFilter<String> bloomFilter = new BloomFilter(BITSET_SIZE, EXPECTED_NUM_OF_ELEMENTS);
+
+        int charLen = chars.length;
+
+        for (int j = 0; j < charLen; j++) {
+            bloomFilter.add(chars[j] + LibConstants.EMPTY_STRING + j);
+        }
+
+        out.add(base64.encodeToString(bloomFilter.getBitSet().toByteArray()));
+        bloomFilter.clear();
+
+        return out;
+    }
+}
+
