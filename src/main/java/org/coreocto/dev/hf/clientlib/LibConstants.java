0333bf38e947b280f59c54956b441c89ec5337f0
coreocto <coreocto@hotmail.com>
2018-02-22 14:43:28 +0800

    Include exclamation mark in split chars

--- src/main/java/org/coreocto/dev/hf/clientlib/LibConstants.java
+++ src/main/java/org/coreocto/dev/hf/clientlib/LibConstants.java
@@ -7,7 +7,7 @@ public class LibConstants {
     public static final String REGEX_SPACE = "\\s";
     public static final String ENCODING_UTF8 = "UTF-8";
     public static final String REGEX_NON_WORD = "\\W";
-    public static final String REGEX_SPLIT_CHARS = "[\\r?\\n|\\p{Blank}|\\p{Cntrl}]+";
+    public static final String REGEX_SPLIT_CHARS = "[\\r?\\n|\\p{Blank}|\\p{Cntrl}|!]+";
     public static final String EMPTY_STRING = "";
     public static final Charset CHARSET_UTF8 = Charset.forName(ENCODING_UTF8);
 }
